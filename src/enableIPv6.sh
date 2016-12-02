26185e68acf7d998d3f1d9785e9acb2d00a99827
Phil Porada <philporada@gmail.com>
2016-12-02 03:56:30 +0000

    Swapped echo to printf because it it POSIX portable and a bash builtin. http://wiki.bash-hackers.org/commands/builtin/printf

--- src/enableIPv6.sh
+++ src/enableIPv6.sh
@@ -23,52 +23,52 @@ for region in "${REGIONLIST[@]}"; do
   # Blank the VPC array for each new region
   VPCLIST=()
 
-  echo "===="
-  echo "Checking ${REGION} for VPC IDs."
+  printf "====\n"
+  printf "Checking ${REGION} for VPC IDs.\n"
 
   for vpcid in `aws ec2 describe-vpcs --output json --region ${REGION} | grep -ohE "\w*vpc-[a-zA-Z0-9]{8}"`; do
     VPCLIST+=("${VPCID}")
-    echo "${REGION}: ${VPCID}"
+    printf "${REGION}: ${VPCID}\n"
   done
 
-  echo ""
+  printf '\n'
 
   for vpcid in "${VPCLIST[@]}"; do
-    echo "[Enabling IPv6 on ${VPCID}]"
+    printf "[Enabling IPv6 on ${VPCID}]\n"
 
-    echo "Associating IPv6 CIDR Block to ${VPCID}."
+    printf "Associating IPv6 CIDR Block to ${VPCID}.\n"
     aws ec2 associate-vpc-cidr-block --output json --region ${REGION} --vpc-id ${VPCID} --amazon-provided-ipv6-cidr-block
 
     TARGETIGW=$(aws ec2 describe-internet-gateways --output json --region ${REGION} | grep -B 10 "${VPCID}" | grep -oh "\w*igw-\w*")
 
     # One IGW per VPC, but many possible Route Tables per VPC.
     for routetable in `aws ec2 describe-route-tables --output json --region ${REGION} | grep -B 1 "${VPCID}" | grep -oh "\w*rtb-\w*"`; do
-      echo "Updating ${ROUTETABLE} with IPv6 default route to ${TARGETIGW}."
+      printf "Updating ${ROUTETABLE} with IPv6 default route to ${TARGETIGW}.\n"
       aws ec2 create-route --output json --region ${REGION} --route-table-id ${ROUTETABLE} --gateway-id ${TARGETIGW} --destination-ipv6-cidr-block "::/0"
     done
 
     # The following updates all Security Groups in the VPC to allow IPv6 traffic outbound. This occurs on every Security Group, so it may not fit everyone's use case. 
     # Disable if you don't want some Security Groups to allow access to the Internet for IPv6 traffic. Generally we do, so it is enabled by default.
     for securitygroup in `aws ec2 describe-security-groups --output json --region ${REGION} | grep -A 3 "${VPCID}" | grep -oh "\w*sg-\w*"`; do
-      echo "Updating ${SECURITYGROUP} to allow IPv6 traffic. $(aws ec2 describe-security-groups --output json --region ${REGION} --group-id "${SECURITYGROUP}" | grep GroupName)"
+      printf "Updating ${SECURITYGROUP} to allow IPv6 traffic. $(aws ec2 describe-security-groups --output json --region ${REGION} --group-id "${SECURITYGROUP}" | grep GroupName)\n"
       aws ec2 authorize-security-group-ingress --output json --region ${REGION} --group-id ${SECURITYGROUP} --ip-permissions '[{"Ipv6Ranges":[{"CidrIpv6":"::/0"}], "IpProtocol":"-1"}]'
     done
 
     # Verify that the VPC got an IPv6 assignment.
     aws ec2 describe-vpcs --output json --region ${REGION} --vpc-id ${VPCID} | grep -A 8 "Ipv6CidrBlockAssociationSet"
 
-    echo "Remember to allocate subnets for each VPC or you won't be using any of your assigned CIDR block."
+    printf "Remember to allocate subnets for each VPC or you won't be using any of your assigned CIDR block.\n"
     # aws ec2 associate-subnet-cidr-block --output json --region ${REGION} --subnet-id ${TARGETSUB} --ipv6-cidr-block <::/64>
     # aws ec2 modify-subnet-attribute --output json --region ${REGION} --subnet ${TARGETSUB} --assign-ipv6-address-on-creation
     # aws ec2 modify-subnet-attribute --output json --region ${REGION} --subnet ${TARGETSUB} --map-public-ip-on-launch
 
-    echo ""
-    echo "----"
-    echo "${REGION}: ${VPCID} is now enabled for IPv6!"
-    echo "----"
+    printf '\n'
+    printf "----\n"
+    printf "${REGION}: ${VPCID} is now enabled for IPv6!\n"
+    printf "----\n"
 
   done
 
-  echo "${REGION} Completed."
-  echo "===="
+  printf "${REGION} Completed.\n"
+  printf "====\n"
 done
